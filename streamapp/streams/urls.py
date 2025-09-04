f6d0037381dbce6a2e138d2393d0beae79714b87
srinivaslikhith <likhithsrinivas9@gmail.com>
2025-09-04 12:15:40 -0700

    correct urlpath

--- streamapp/streams/urls.py
+++ streamapp/streams/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("api/register/", views.register, name="register"),
+    path("register/", views.register, name="register"),
     path("stream_url/", views.stream_url, name="stream_url"),
     path("stream_start/", views.stream_start, name="stream_start"),
     path("stream_stop/", views.stream_stop, name="stream_stop"),
