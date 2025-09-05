f594662e6a08a2f11271ea5f132c9a18bcc4f861
srinivaslikhith <likhithsrinivas9@gmail.com>
2025-09-05 10:38:15 -0700

    add login

--- streamapp/streams/urls.py
+++ streamapp/streams/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path("register/", views.register, name="register"),
+    path("login/", obtain_auth_token),
     path("stream_url/", views.stream_url, name="stream_url"),
     path("stream_start/", views.stream_start, name="stream_start"),
     path("stream_stop/", views.stream_stop, name="stream_stop"),
