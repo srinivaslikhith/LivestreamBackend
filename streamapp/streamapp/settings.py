e9550be5b8d9c28253e2357933238638dafda7dc
srinivaslikhith <likhithsrinivas9@gmail.com>
2025-09-05 10:46:03 -0700

    add streamhost

--- streamapp/streamapp/settings.py
+++ streamapp/streamapp/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+STREAM_HOST = "52.15.114.254"  # or your EC2 public IP / domain
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
